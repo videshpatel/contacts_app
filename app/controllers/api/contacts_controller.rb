109dc5ed4139cd994108e3719514abc7041aa897
“Videsh <videsh.patel@gmail.com>
2019-03-04 19:32:18 -0600

    added show page.

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -2,7 +2,13 @@ class Api::ContactsController < ApplicationController
   
   def index
     @contacts = Contact.all
-
     render 'index.json.jbuilder'
   end
+
+  def show
+    id = params[:id]
+    @contact = Contact.find_by(id: id)
+    render 'show.json.jbuilder'  
+  end
+
 end
