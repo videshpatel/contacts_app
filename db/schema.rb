92c44bbe73e19bab27223460751a342238ee60f8
“Videsh <videsh.patel@gmail.com>
2019-03-18 21:10:50 -0500

    Added a ContactGroup model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_013442) do
+ActiveRecord::Schema.define(version: 2019_03_19_020924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contact_groups", force: :cascade do |t|
+    t.integer "contact_id"
+    t.integer "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contact_lists", force: :cascade do |t|
     t.string "name"
